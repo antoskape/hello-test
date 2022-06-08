e5365b08dbedd6dbf29c764d1d5e52ba3aca784c
Peter Antoska <peter.antoska@gmail.com>
2022-06-08 22:26:59 +0200

    Show time stamp

--- date.py
+++ date.py
@@ -6,3 +6,6 @@ FirstStartTime = datetime.now()
 # format:  YYYY-MM-DD hh:mm:ss.msm like 2016-10-04 10:09:35.699
 mydate = FirstStartTime.strftime("%Y") + "-" + FirstStartTime.strftime("%m") + "-" + FirstStartTime.strftime("%d")
 print(mydate)
+
+# Uprava 8.6.2022 - zobrazenie casu
+print(FirstStartTime)
